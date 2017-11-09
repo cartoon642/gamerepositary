15d4c828c2692317f076418e75e19c12633c794e
Axel Satariano <a100230@mcast.org.mt>
2017-11-09 10:53:37 +0100

    added games in repositary

---
+++ BrickBreaker/BrickBreaker/Assets/Scripts/MusicPlayerScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicPlayerScript : MonoBehaviour {
+
+    // Use this for initialization
+    static MusicPlayerScript instance = null;
+	void Start () {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
